56c4d8e61e87dae1583f69bff2570fe4a5e3bf44
yaat2006 <yaat2006@metabuild.co.kr>
2019-06-03 09:41:18 +0900

    first commit

---
+++ public/precache-manifest.83854b748214e9e98a561d598eeeb569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a748163499ee9c3349dd",
+    "url": "/js/chunk-vendors.c59390bc.js"
+  },
+  {
+    "revision": "ec80026ba31730636ac1",
+    "url": "/js/app.2f546ac4.js"
+  },
+  {
+    "revision": "003bf2cda7d7b50ebb77",
+    "url": "/js/about.917d6416.js"
+  },
+  {
+    "revision": "3f4008231d1c21c46bb8470e37cbe91a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ec80026ba31730636ac1",
+    "url": "/css/app.d8d645d5.css"
+  }
+];
